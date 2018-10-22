300fa46e56ffaf90d979a891c911589c6d4c42c5
frizzy5 <jevoycharvis@yahoo.com>
2018-10-22 15:45:49 -0500

    edit2

--- maze.js
+++ maze.js
@@ -1,4 +1,4 @@
- 
+ var tracker= true;
 function boun(){
 	var top= document.getElementById("boundary1");
 	if (top.addEventListener){
@@ -15,10 +15,10 @@ function boun(){
  	var cr= document.querySelectorAll('.boundary');
  	for(var i=1;i<cr.length;i++){
  		if(cr[i]==this.valueOf()){
+ 			tracker=false;
  			cr[i].setAttribute("style", "background-color: #ff8888;");
  		}
  	}
- 	
 
 }
 
@@ -28,16 +28,32 @@ function boun(){
  	var cr= document.querySelectorAll('.boundary');
  	for(var x= 1; x<cr.length; x++){
  		cr[x].addEventListener("mouseover",wall2);
-
-
+ 		
  }
 }
- 
+function al(){
+ 		var win=document.getElementById("end");
+ 		if(tracker== true){
+ 			alert(" successful completion of maze ");
+ 		}
+ 		else if (tracker== false){
+ 			alert("You lose");
+
+ 		}
+
+ 	} 
+
+ 	function alwin(){
+ 		var win=document.getElementById("end");
+ 		win.addEventListener('mouseover',al);
+ 	}
+
 
 
 window.onload= function(){
 	
 	boun();
 	allBoun();
+	alwin();
 	
 };
