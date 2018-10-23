87eaff02cab8b254a3c29788beaeac8627fdf815
frizzy5 <jevoycharvis@yahoo.com>
2018-10-23 16:07:08 -0500

    edit3

--- maze.js
+++ maze.js
@@ -1,4 +1,7 @@
  var tracker= true;
+ var count=1;
+
+
 function boun(){
 	var top= document.getElementById("boundary1");
 	if (top.addEventListener){
@@ -13,20 +16,22 @@ function boun(){
  }
  function wall2(){
  	var cr= document.querySelectorAll('.boundary');
- 	for(var i=1;i<cr.length;i++){
+ 	for(var i=0;i<cr.length;i++){
  		if(cr[i]==this.valueOf()){
  			tracker=false;
- 			cr[i].setAttribute("style", "background-color: #ff8888;");
- 		}
+ 			for(var x=0;x<cr.length-1;x++){
+ 				cr[x].setAttribute("style", "background-color: #ff8888;");
+ 			}
+ 		
  	}
-
+  }
 }
 
  
 
  function allBoun(){
  	var cr= document.querySelectorAll('.boundary');
- 	for(var x= 1; x<cr.length; x++){
+ 	for(var x= 0; x<cr.length; x++){
  		cr[x].addEventListener("mouseover",wall2);
  		
  }
@@ -34,10 +39,10 @@ function boun(){
 function al(){
  		var win=document.getElementById("end");
  		if(tracker== true){
- 			alert(" successful completion of maze ");
+ 			document.getElementById("status").innerHTML = " successful completion of maze ";
  		}
  		else if (tracker== false){
- 			alert("You lose");
+ 			document.getElementById("status").innerHTML = 'You lose';
 
  		}
 
@@ -47,13 +52,62 @@ function al(){
  		var win=document.getElementById("end");
  		win.addEventListener('mouseover',al);
  	}
+ 		
+ 	function start(){
+ 		var s=document.getElementById('start');
+ 		if(tracker== false|| tracker== true){
+ 			count=0;
+ 			tracker= true
+ 			var cr= document.querySelectorAll('.boundary');
+ 			for(var x= 0; x<cr.length-1; x++){
+ 				cr[x].setAttribute("style", "background-color: #eeeeee;");
+ 				
+
+ 			}
+ 			//location.reload();
+ 			document.getElementById("status").innerHTML = " Move your mouse over the S to begin";
+ 			main();
+
+ 		}
+ 	}
+ 	function startEvent(){
+ 		var st=document.getElementById('start');
+ 		var sy=st.addEventListener('click',start);
+ 		
+ 	}
+
+ 	function leave(){
+ 		document.getElementById("status").innerHTML = 'You lose Cheater';
+
+
+ 	}
+
+ 	function cheatEevent(){
+ 		var lev=document.getElementById("maze");
+ 		lev.addEventListener('mouseleave',leave);
+
+
+ 	}
+
+function main(){
+	if(count==1){
+		startEvent();
+	}
+	else if (count==0) {
+	cheatEevent();
+	boun();
+	allBoun();
+	startEvent();
+	alwin();
+
+	}
+}
 
 
 
 window.onload= function(){
+	main();
 	
-	boun();
-	allBoun();
-	alwin();
+
 	
 };
